af95be4fbc598930192bb5ac7c93d30ab399227e
Andy Hoffman <antibland@gmail.com>
2020-02-19 23:59:47 -0800

    Upgrade to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,67 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
+const { watch, src, dest, series, parallel } = require("gulp");
+const browserSync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const del = require("del");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
-// CSS
-gulp.task('build-css', function() {
-  gulp.src('./css/style.css')
-    .pipe(plumber())
-    .pipe(autoprefixer({
-      browsers: [
-        '> 1%',
-        'last 2 versions',
-        'firefox >= 40',
-        'safari >= 7',
-        'IE 11'
-      ],
-      cascade: false
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./prod'));
-});
+const config = {
+  app: {
+    css: "./css/**/*.css",
+    html: "./*.html",
+    root: "./"
+  },
+  dist: {
+    base: "./dist/"
+  }
+};
 
-// Watching
-gulp.task('watch', function() {
-  gulp.watch('./css/*.css', ['build-css']);
-});
+function cssTask(done) {
+  src(config.app.css)
+    .pipe(rename({ suffix: ".bundle" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest(config.dist.base));
+  done();
+}
 
-// Commands
-gulp.task('default', [
-  'build-css', 'watch'
-]);
+// function fontTask(done) {
+//   src(config.app.fonts).pipe(dest(config.dist.fonts));
+//   done();
+// }
+
+// function imagesTask(done) {
+//   src(config.app.images).pipe(dest(config.dist.images));
+//   done();
+// }
+
+// function templateTask(done) {
+//   src(config.app.html).pipe(dest(config.app.root));
+//   done();
+// }
+
+function watchFiles() {
+  watch(config.app.css, series(cssTask, reload));
+  watch(config.app.html, series(reload));
+}
+
+function liveReload(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  done();
+}
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function cleanUp() {
+  return del([config.dist.base]);
+}
+
+exports.dev = parallel(cssTask, watchFiles, liveReload);
+exports.build = series(cleanUp, parallel(cssTask));
